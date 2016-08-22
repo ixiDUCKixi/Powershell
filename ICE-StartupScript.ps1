e841b3ded7e25a777dfb69d4c1ced83ca2db16f8
ixiDUCKixi <darylmoxham@gmail.com>
2016-08-22 12:40:07 -0500

    Update ICE-StartupScript.ps1
    Authored by /u/TotallyNotBlunt

--- ICE-StartupScript.ps1
+++ ICE-StartupScript.ps1
@@ -1,1 +1,1 @@
-Get-Module -ListAvailable | Where-Object {$_.Path -like 'C:\Scripts\*'} | Update-Module -Force -Confirm:$false -ErrorAction SilentlyContinue
+Get-Module -ListAvailable | Where-Object {$_.Path -like 'C:\Scripts\*'} | Update-Module -Force -Confirm:$false -ErrorAction SilentlyContinue 
