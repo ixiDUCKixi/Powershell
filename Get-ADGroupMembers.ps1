a705266d2ef3b008c4d6f4b402e733d8aefd7667
ixiDUCKixi <darylmoxham@gmail.com>
2016-08-25 16:20:43 -0500

    Update Get-ADGroupMembers.ps1
    Added a function to create the directory "ADAdmins" if it doesn't exist.  Modify line 13 to change the folder name if needed.  If you need to save to a different drive, change line 19 also.

--- Get-ADGroupMembers.ps1
+++ Get-ADGroupMembers.ps1
@@ -2,7 +2,7 @@
 #
 #    This script will parse a single group for all 
 #    group memebers and nested groups and export a CSV
-#    to C:\Admins.  Edit line 16 to change the export path
+#    to C:\Admins.  Edit line 13 to change the export path
 #    if required.  
 #
 #    You may need to run this multiple times one each group
@@ -10,18 +10,34 @@
 #
 #-----------------------------------------------------------
 
+$Outputpath = "ADAdmins";
 
+$Currentlocation = Get-Location
+$PathTest = Test-Path -path "C:\$($Outputpath)";
 $Server = Read-Host -Prompt "Input the FQDN for the Domain (Example:  r1.aig.net)"
-$ADGroupName = Read-Host -Prompt "Input the Group (Example:  Administrators)"
-$CsvPath = "c:\ADAdmins\$($Server)_Administrators_$(Get-Date -Format ddMMyyyy).csv"
+$ADGroupName = Read-Host -Prompt "Input the Group (Example:  Administrators or Domain Admins)"
+$CsvPath = "c:\$($outputpath)\$($Server)_$($ADGroupName)_$(Get-Date -Format ddMMyyyy).csv"
 #$ADGroupName = "Administrators"
 $Output = @()
+
+if ($pathtest -eq $FALSE )
+	{
+	cd "C:\";
+	mkdir $($Outputpath)
+	write-host "Output path $($Outputpath) created"
+	CD $currentlocation
+	}
+elseif ($pathtest -eq $TRUE )
+	{ 
+	write-host  "Output path exists, Continuing script"
+	}
+
 
 $ADGroupMembers_Users = Get-ADGroupMember $ADGroupName -Recursive -Server $Server
 $ADGroupMembers_Groups = Get-ADGroupMember $ADGroupName -Server $Server | Where-Object {$_.ObjectClass -eq "group"}
 
 foreach ($m in $ADGroupMembers_Users) {
-  $Output += Get-ADUser $m | select Name,SamAccountName,Description,ObjectClass,Enabled
+  $Output += Get-ADUser $m | select Name,GivenName,sn,SamAccountName,Description,Department,employeeID,ObjectClass,Enabled
 }
 
 foreach ($m in $ADGroupMembers_Groups) {
